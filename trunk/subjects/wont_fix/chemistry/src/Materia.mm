1f85ca60e93ba37d0161fc800fee37e1aaef3731
foguinho.peruca@gmail.com <foguinho.peruca@gmail.com@8ec260ef-71c7-b7d0-5180-dd010961e1d1>
2011-08-26 12:57:16 +0000

    Migration From Git to SVN. iMigration successful!
    git-svn-id: https://math-ufscar-2011-1.googlecode.com/svn@2 8ec260ef-71c7-b7d0-5180-dd010961e1d1

---
+++ trunk/subjects/wont_fix/chemistry/src/Materia.mm
@@ -0,0 +1,32 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1300755810645" ID="ID_785031862" MODIFIED="1300757601881" TEXT="Mat&#xe9;ria">
+<node CREATED="1300757298776" ID="ID_410966717" MODIFIED="1300757596550" POSITION="right" TEXT="&#xe9; uniforme?">
+<icon BUILTIN="help"/>
+<node CREATED="1300757318859" ID="ID_1453761116" MODIFIED="1300757359405" TEXT="MIstura heterog&#xea;nea">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1300757330763" ID="ID_574592859" MODIFIED="1301260837905" TEXT="sim... &#xe9; homog&#xea;nea">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1300757396069" ID="ID_135790052" MODIFIED="1300757590439" TEXT="tem composi&#xe7;&#xe3;o vis&#xed;vel">
+<icon BUILTIN="help"/>
+<node CREATED="1300757438302" ID="ID_1968308177" MODIFIED="1301260765482" TEXT="sim.. &#xe9; mistura homog&#xea;nea (solu&#xe7;&#xe3;o)">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1300757495817" ID="ID_1891797305" MODIFIED="1300757512654" TEXT="n&#xe3;o... subst&#xe2;ncia pura">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1300757522513" ID="ID_72429126" MODIFIED="1300757588161" TEXT="pode ser decomposto em subst + simples?">
+<icon BUILTIN="help"/>
+<node CREATED="1300757564298" ID="ID_766523848" MODIFIED="1301260862453" TEXT="sim... &#xe9; subst&#xe2;ncia composta">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1300757550529" ID="ID_607426052" MODIFIED="1301260856513" TEXT="n&#xe3;o... &#xe9; subst&#xe2;ncia simples">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
