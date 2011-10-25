6270133c8be85a685b08b69333cc9004e18a5c4f
foguinho.peruca@gmail.com <foguinho.peruca@gmail.com@8ec260ef-71c7-b7d0-5180-dd010961e1d1>
2011-10-25 03:16:23 +0000

    refs #36686
    * Implementado cálcujlo dp produto vetoria;
    * Implementado o cálculo do produto escalar;
    
    
    git-svn-id: https://math-ufscar-2011-1.googlecode.com/svn@20 8ec260ef-71c7-b7d0-5180-dd010961e1d1

--- trunk/subjects/344982_PROGRAMACAO_E_ALGORITMOS/assigment_at_ufscar/matrix.c
+++ trunk/subjects/344982_PROGRAMACAO_E_ALGORITMOS/assigment_at_ufscar/matrix.c
@@ -1,35 +1,64 @@
 /**
  * (C) copyright 2011 - Jefferson Campos - foguinho.peruca@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * This program is free software under GPLv3 terms.
- * Este programa implementa a lista de treino 01.
- * Para compilar: gcc -Wall matrix.c -o matrix
+ * 
+ * Este programa implementa a atividade proposta em sala de aula - OCT-24-2011 - 344982_PROGRAMACAO_E_ALGORITMOS.
+ * Para compilar: gcc -Wall matrix.c -o matrix -lm
+ * 
  */
 
 # include <stdio.h>
 # include <stdlib.h>
+# include <math.h>
 
 # define ESCALAR 20
+# define SIZE 5
+# define PI 3.1415
+
+float
+norma(int X[])
+{
+  int i, norma = 0;
+  for (i = 0; i < SIZE; i++)
+    {
+      norma += pow(X[i], 2);
+    }
+  return sqrt(norma);
+}
+
+
 void
 compare_matrix()
 {
-  int a[5], b[5], i, j;
+  int A[5], B[5], i, j;
 
   for (i = 0, j = 4; i < 5; i++, j--)
     {
-      a[i] = i;
-      printf("i: %i position. Value is: %i\n", i, a[i]);
-      b[j] = i;
-      printf("j: %i position. Value is: %i\n", j, b[j]);
+      A[i] = i;
+      printf("i: %i position. Value is: %i\n", i, A[i]);
+      B[j] = i;
+      printf("j: %i position. Value is: %i\n", j, B[j]);
     }
 
   for (i = 0, j = 4; i < 5; i++, j--)
     {
-      printf("i: %i ; j: %i\n", a[i], b[j]);
+      printf("i: %i ; j: %i\n", A[i], B[j]);
     }
 }
 
 void
-verify_matrix(int a[], int b[])
+verify_matrix(int A[], int B[])
 {
   int i, j;
 
@@ -37,41 +66,44 @@ verify_matrix(int a[], int b[])
 
   for (i = 0, j = 4; i < 5; i++, j--)
     {
-      printf("i: %i position. Value is: %i\n", i, a[i]);     
-      printf("j: %i position. Value is: %i\n", j, b[j]);
+      printf("i: %i position. Value is: %i\n", i, A[i]);
+      printf("j: %i position. Value is: %i\n", j, B[j]);
     }
 }
 
 void
-by_escalar(int escalar, int a[])
+by_escalar(int escalar, int A[])
 {
   int i;
 
-  printf("===== Calc by escalar!! =====\n");
+  printf("\n===== Calc by escalar!! =====\n");
 
-  for (i = 0; i < 5; i++)
+  for (i = 0; i < SIZE; i++)
     {
-      printf("i: %i position. Value is: %i\n", i, a[i] * escalar);     
+      printf("i: %i position. Value is: %i\n", i, A[i] * escalar);
     }
 }
 
+/*
+ * Forma do calculo: A * B --> (a1 * b1) + (a2 * b2) + (a3 + b3)
+ */
 void
-escalar(int a[], int b[])
+produto_escalar(int A[], int B[])
 {
-  int i, c[5], final;
+  int i, C[SIZE], final;
   final =  0;
 
-  printf("===== Produto escalar =====\n");
+  printf("\n===== Produto escalar =====\n");
 
-  for (i = 0; i < 5; i++)
+  for (i = 0; i < SIZE; i++)
     {
-      c[i]  = a[i] * b[i];
-      printf("a[%i](%i) * b[%i](%i) = %i\n",i, a[i], i, b[i], c[i]);
+      C[i]  = A[i] * B[i];
+      printf("A[%i](%i) * B[%i](%i) = %i\n",i, A[i], i, B[i], C[i]);
     }
 
-  for (i = 0; i < 5; i++)
+  for (i = 0; i < SIZE; i++)
     {
-      final += c[i];
+      final += C[i];
     }
 
   printf("Final is: %i!!\n", final);
@@ -79,20 +111,50 @@ escalar(int a[], int b[])
 
 /**
  * too boring to implement... :P
- * formula: ||V * W || = ||V|| ||W|| sen
+ * formula: ||V * W || = ||V|| ||W|| cos "θ"
  */
 void
-produto_vetorial(int a[], int b[])
+produto_vetorial(int A[], int B[], double ang)
 {
-  /* int i c[][]; */
+  double normaA, normaB, prod_vet;
 
-  /* printf("Produto vetorial!!\n"); */
+  printf("\n===== Produto Vetorial =====\n");
 
-  /* for (i = 0; i < 5; i++) */
-  /*   { */
-      
-  /*     printf("a[%i](value: %i) * b[%i](value: %i) = %i\n", i, a[i], i, b[i],  c[i][i]);      */
-  /*   } */
+  normaA = norma(A);
+  normaB = norma(B);
+
+  prod_vet = normaA * normaB * sin (ang * PI/180);
+
+  printf("normaA: %.2f\n", normaA);
+  printf("normaB: %.2f\n", normaB);
+  printf("sin: %.2f\n", cos (ang * PI/180));
+  printf("prod_vet: %.2f\n", prod_vet);
+}
+
+/**
+ * formula: sen "θ" = ||V * W || = ||V|| ||W||
+ */
+void
+calc_ang(int A[], int B[])
+{
+  double normaA, normaB, ang;
+
+  printf("\n===== Calc Ang =====\n");
+
+  normaA = norma(A);
+  normaB = norma(B);
+
+  if (normaA == 0 || normaB ==0)
+    {
+      printf("\nError! norma < 0!!\n");
+      return;
+    }
+
+  ang = (normaA * normaB);
+
+  printf("normaA: %.2f\n", normaA);
+  printf("normaB: %.2f\n", normaB);
+  // printf("sin: %.2f\n", sin (ang * PI/180));
 }
 
 int
@@ -100,9 +162,9 @@ main (void)
 {
   //compare_matrix();
 
-  int a[5], b[5], i, j;
+  int a[SIZE], b[SIZE], i, j;
 
-  for (i = 0, j = 4; i < 5; i++, j--)
+  for (i = 0, j = 4; i < SIZE; i++, j--)
     {
       a[i] = i;
       /* printf("i: %i position. Value is: %i\n", i, a[i]); */
@@ -112,8 +174,8 @@ main (void)
 
   verify_matrix(a, b);
   by_escalar(ESCALAR, a);
-  escalar(a, b);
-  //  produto_vetorial(a, b);
+  produto_escalar(a, b);
+  produto_vetorial(a, b, 60.0);
   /* find_ang(a, b); */
 
   return 0;
